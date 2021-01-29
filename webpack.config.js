b3e6659c4659a340a4d605533b123a07d30abc0f
XiaoPan <2489964425@qq.com>
2021-01-29 12:50:22 +0800

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path')
 module.exports = {
   mode: 'development',  //production(发布)，development(开发)
   entry: './src/index.js',
   output: {
     filename: '[name].[contenthash].js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '首页',
+    template: 'src/assets/index.html'
+  })]
 };
