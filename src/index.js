6321062ec9ae99234da74ea6446980c7d97dc10a
XiaoPan <2489964425@qq.com>
2021-01-30 17:46:50 +0800

    文件加载 file-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import x from './x.js'
+import png from './assets/me.jpg'
+const div = document.getElementById('app')
+div.innerHTML = `
+<img src="${png}">`
 console.log('hi')
 console.log('heheheh')
