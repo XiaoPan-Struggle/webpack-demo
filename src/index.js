c2749f8feeb2f0878eacd39d2fb4f2e4a0a10236
XiaoPan <2489964425@qq.com>
2021-01-30 18:33:23 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ div.innerHTML = `
 <img src="${png}">`
 console.log('hi')
 console.log('heheheh')
+
+const button = document.createElement('button')
+button.innerHTML = '懒加载'
+button.onclick = () => {
+  const promise = import('./lazy.js')
+  promise.then((module) => {
+    module.default()
+  })
+}
+div.appendChild(button)
